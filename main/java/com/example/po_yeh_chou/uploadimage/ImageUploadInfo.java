b1d290bbbf81ae826c4e411c7c987d65e0a98b56
choujason <jasonya2002@gmail.com>
2017-10-23 23:28:24 +0800

    Initial commit

---
+++ main/java/com/example/po_yeh_chou/uploadimage/ImageUploadInfo.java
@@ -0,0 +1,31 @@
+package com.example.po_yeh_chou.uploadimage;
+
+/**
+ * Created by Po_Yeh_Chou on 23/10/2017.
+ */
+
+public class ImageUploadInfo {
+
+    public String imageName;
+
+    public String imageURL;
+
+    public ImageUploadInfo() {
+
+    }
+
+    public ImageUploadInfo(String name, String url) {
+
+        this.imageName = name;
+        this.imageURL= url;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+}
